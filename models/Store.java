91a8020dcad3fdd2349c72c69d53428223fa8c65
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-20 11:54:20 -0700

    added action function

--- models/Store.java
+++ models/Store.java
@@ -4,5 +4,40 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Store {
-    private Collection<Movie> movies = new ArrayList<Movie>();
+    private ArrayList<Movie> movies;
+
+    public Store() {
+        this.movies = new ArrayList<Movie>();
+    }
+
+    public Movie getMovie(int index){
+        return new Movie(this.movies.get(index));
+    }
+
+    public void setMovie(int index, Movie movie ){
+        this.movies.set(index, new Movie(movie));
+    }
+
+    public void addMovie(Movie movie) {
+        this.movies.add(new Movie(movie));
+    }
+
+    public void action(String name, String action){
+        for (int i = 0; i < this.movies.size(); i++) {
+            if (this.movies.get(i).getName().equals(name)){
+                switch(action){
+                    case "sell": this.movies.remove(i);
+                    break;
+                    case "rent": this.movies.get(i).setAvailable(false);
+                    break;
+                    case "return": this.movies.get(i).setAvailable(true);
+                    break;
+                }
+            }
+        }
+    }
+
+    
+
+
 }
